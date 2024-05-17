c0ae62286bd857d1d8fe76c9d0f7509b1d459fb9
teusma <78906680+mateusmacciel@users.noreply.github.com>
2024-05-17 19:44:31 -0300

    Update multithreadchar.java

--- multithreadchar.java
+++ multithreadchar.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.Map; //
 
 class ContadorCaracteres {
     public static void main(String[] args) {
